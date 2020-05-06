80d28842fd94ed04c05f0abdbdc9ca482f4f017a
hanxingchao1269202647 <63250387+hanxingchao1269202647@users.noreply.github.com>
2020-05-06 10:51:57 +0800

    Update B.java
    的事发地时

--- src/main/java/com/cn/taotao/B.java
+++ src/main/java/com/cn/taotao/B.java
@@ -5,8 +5,8 @@ public class B {
 	public static void main(String[] args) {
 		System.out.println("dsdadfdfd");
 		
-		System.out.println("多少度");
-		System.out.println("1223少度");
+		System.out.println("多1少度");
+		System.out.println("12231少度");
 		System.out.println("dsda2323dfdfd");
 		System.out.println("dsdadfdfd");
 	}
